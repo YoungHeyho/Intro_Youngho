d577de390e89d0365079e8ac78005209630ee6bc
YoungHeyho <162882003+YoungHeyho@users.noreply.github.com>
2024-05-31 23:26:33 +0900

    Update HoJs.js

--- HoJs.js
+++ HoJs.js
@@ -6,4 +6,15 @@ $(document).ready(function(){
         $('body>div').addClass('None').removeClass('Block');
         $(target).addClass('Block').removeClass('None');
     });
+    $('#menu a').click(function(event){
+        event.preventDefault();
+        var target = $(this).attr('href');
+        
+        $('body>div').addClass('None').removeClass('Block');
+        $(target).addClass('Block').removeClass('None');
+    });
+    
+    $('.menubut').click(function(){
+        $('#menu').slideToggle('500');
+    });
 });
